635307ce5254a432bb00aebdafb8ccf9a8a0a4e8
Robin <robin.natale@swankyagency.com>
2023-08-03 10:08:58 +0100

    Import state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 // Import Styles
 import "./styles/app.scss"
@@ -7,10 +7,17 @@ import "./styles/app.scss"
 import Player from "./components/Player";
 import Song from "./components/Song";
 
+// Import Util
+import data from "./util";
+
 function App() {
+  // State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );

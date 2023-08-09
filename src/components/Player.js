8b071b8ec9eaa1e0bbe2d2bda862fe108e37afe7
Robin <robin.natale@swankyagency.com>
2023-08-09 10:16:16 +0100

    Make slider draggable and update value on audio

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,17 +1,20 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 // Import the font awesome package
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // Import specific icons
-import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
-const Player = ({ currentSong , isPlaying , setIsPlaying }) => {
-
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   // Ref
   const audioRef = useRef(null);
 
   // Event handlers
-  const playSongHandler = () => {  
-    if (isPlaying){
+  const playSongHandler = () => {
+    if (isPlaying) {
       audioRef.current.pause();
       setIsPlaying(!isPlaying);
     } else {
@@ -24,13 +27,18 @@ const Player = ({ currentSong , isPlaying , setIsPlaying }) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
     console.log(duration);
-    setSongInfo({...songInfo, currentTime: current, duration: duration})
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
   };
 
   const getTime = (time) => {
     return (
       Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
     );
+  };
+
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({...songInfo, currentTime: e.target.value} )
   };
 
   // State
@@ -43,15 +51,35 @@ const Player = ({ currentSong , isPlaying , setIsPlaying }) => {
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input min={0} max={songInfo.duration} value={songInfo.currentTime} type="range" />
+        <input
+          min={0}
+          max={songInfo.duration}
+          value={songInfo.currentTime}
+          onChange={dragHandler}
+          type="range"
+        />
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-play" size="2x" icon={faAngleLeft} />
-        <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
-        <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
       </div>
-      <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 };
